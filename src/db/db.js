22838e7666cdcbf879d240892f9a3d8fe0040ad9
sharvanrajput <sharvanrajput098@gmail.com>
2025-11-08 13:57:02 +0530

    just add new things

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/blogs`)
+        await mongoose.connect(`${process.env.MONGODB_URL}/blogs`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 10000,
+        })
         console.log("db connected successfully ")
     } catch (error) {
         console.log("db not Connected")
